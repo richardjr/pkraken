2d3bac82f5550121bd892ede5031e2e077acb9bb
Richard Reynolds <xrouter@gmail.com>
2024-03-21 10:31:09 +0000

    deployment fixes

--- kraken_site/settings.py
+++ kraken_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-98xav$i799=egnt^dxx!7_8as6xlve)mehko9q7s&l6no92ly8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
